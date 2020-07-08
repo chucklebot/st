7c37ce009182724452fcf4aa311730c17945aee2
Paul Collins <paul@ondioline.org>
2020-07-09 10:37:30 +1200

    paste selection with Mod4+space

--- config.h
+++ config.h
@@ -198,6 +198,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+	{ MODKEY,               XK_space,       selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
